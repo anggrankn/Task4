65856c062f8b93572ddabf6cf5f7430db412bf0d
Angelina Grankina <angelina.grankina200214@mail.ru>
2020-10-28 20:07:44 +0300

    task4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -17,7 +17,7 @@ public class Main {
     public static void readPythagoreanNumbers() {
         int maxNumber = 20;
         for (int i = 1; i <= maxNumber; i++) {
-            for (int j = 1; j <=maxNumber; j++) {
+            for (int j = 1; j <=maxNumber && (i != j); j++) {
                 double z = Math.sqrt(i*i+j*j);
                 if (getRoundingDown(z) && z <= maxNumber) {
                     displayPythagoreanNumbersToTwenty(i, j,(int) z);
