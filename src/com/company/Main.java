a03ce5a5670104fe2f30931d2c4bb943bc2645f0
Angelina Grankina <angelina.grankina200214@mail.ru>
2020-10-30 19:37:11 +0300

    task4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,21 +6,17 @@ public class Main {
        readPythagoreanNumbers();
     }
 
-    public static void displayPythagoreanNumbersToTwenty(int firstNumber, int secondNumber, int thirdNumber) {
+    public static void displayPythagoreanNumbers(int firstNumber, int secondNumber, int thirdNumber) {
         System.out.printf("%d %d %d\n", firstNumber, secondNumber, thirdNumber);
-    }
-
-    public static boolean getRoundingDown(double z) {
-        return Math.floor(z) == z;
     }
 
     public static void readPythagoreanNumbers() {
         int maxNumber = 20;
-        for (int i = 1; i <= maxNumber; i++) {
-            for (int j = 1; j <=maxNumber && (i != j); j++) {
-                double z = Math.sqrt(i*i+j*j);
-                if (getRoundingDown(z) && z <= maxNumber) {
-                    displayPythagoreanNumbersToTwenty(i, j,(int) z);
+        for (int i = 1; i < maxNumber; i++) {
+            for (int j = i; j < maxNumber; j++) {
+                double sqrt = Math.sqrt(i * i + j * j);
+                if ((int) sqrt == sqrt && sqrt <= maxNumber) {
+                    displayPythagoreanNumbers(i, j, (int) sqrt);
                 }
             }
         }
