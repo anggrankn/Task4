44356fd09af6afd55f14f136d009785cd82bb10f
Angelina Grankina <angelina.grankina200214@mail.ru>
2020-10-31 10:16:09 +0300

    task4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,19 +3,27 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int maxNumber = 20;
-        for (int i = 1; i < maxNumber; i++) {
-            for (int j = i; j < maxNumber; j++) {
-                double sqrt = Math.sqrt(i * i + j * j);
-                if ((int) sqrt == sqrt && sqrt <= maxNumber) {
-                    displayPythagoreanNumbers(i, j, (int) sqrt);
-                }
-            }
-        }
+       readPythagoreanNumbers();
     }
 
     public static void displayPythagoreanNumbers(int firstNumber, int secondNumber, int thirdNumber) {
         System.out.printf("%d %d %d\n", firstNumber, secondNumber, thirdNumber);
+    }
+
+    public static boolean getInteger(double sqrt) {
+        return Math.floor(sqrt) == sqrt;
+    }
+
+    public static void readPythagoreanNumbers() {
+        int maxNumber = 20;
+        for (int i = 1; i < maxNumber; i++) {
+            for (int j = i; j < maxNumber; j++) {
+                double sqrt = Math.sqrt(i * i + j * j);
+                if (getInteger(sqrt) && sqrt <= maxNumber) {
+                    displayPythagoreanNumbers(i, j, (int) sqrt);
+                }
+            }
+        }
     }
 }
 
