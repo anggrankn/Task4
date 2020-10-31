aa69d1aa05c6de8271ca6bbba6c2ebd9d5104a3f
Angelina Grankina <angelina.grankina200214@mail.ru>
2020-10-31 09:52:17 +0300

    task4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,14 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-       readPythagoreanNumbers();
-    }
-
-    public static void displayPythagoreanNumbers(int firstNumber, int secondNumber, int thirdNumber) {
-        System.out.printf("%d %d %d\n", firstNumber, secondNumber, thirdNumber);
-    }
-
-    public static void readPythagoreanNumbers() {
         int maxNumber = 20;
         for (int i = 1; i < maxNumber; i++) {
             for (int j = i; j < maxNumber; j++) {
@@ -20,6 +12,10 @@ public class Main {
                 }
             }
         }
+    }
+
+    public static void displayPythagoreanNumbers(int firstNumber, int secondNumber, int thirdNumber) {
+        System.out.printf("%d %d %d\n", firstNumber, secondNumber, thirdNumber);
     }
 }
 
